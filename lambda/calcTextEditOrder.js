d58dd8e3dfcebe095bbad40441184cdf00e33e3a
MonsiSome <62303105+MonsiSome@users.noreply.github.com>
2020-10-08 17:56:32 +0300

    add func ​​​calcTextEditHours &getDeadlineForOrder

--- lambda/calcTextEditOrder.js
+++ lambda/calcTextEditOrder.js
@@ -1,3 +1,4 @@
+// samples of texts to edit (length is counted in naming)
 let text1400 = 'Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. jfsgjasdfg'
 let text1300 = 'Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, пер'
 let text1333 = 'Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але сильно текст не переписуватимемо. Зайвих виправлень не буде. Виправимо всі помилки, приберемо всі дурниці, перефразуємо невдалі місця, але силь'
@@ -8,30 +9,37 @@ const languages = {
   UA: 0.05,
   EN: 0.12
 }
-const typeOfFile = ['.doc', '.docx', '.rtf'];
+const basicTypesOfFile = [".doc", ".docx", ".rtf"];
 const MIN_RU_UKR_COST = 50;
 const MIN_EN_COST = 120;
 const SYMBOLS_RU_UKR_PER_HOUR = 1333;
 const SYMBOLS_EN_PER_HOUR = 333;
-const RISING_PRICE_PERCENT = 20;
-// const ORDER_PROCESSING_TIME_IN_MIN = 30;
+const PERCENT_FOR_EXTRA_RESOURCES = 20;
+const MIN_MINUTES_PER_ORDER = 1 * 60
+const OPEN_ORDER_PROCESSING_TIME_IN_MIN = 30;
+const ONE_HOUR_IN_MIN = 60
+const TWO_HOURS_IN_MIN = 120
+const ZERO_BEFORE_NUM = 10
+// function for formating date
+const countDateDigits = dateToDigit => dateToDigit < ZERO_BEFORE_NUM ? '0' + dateToDigit : dateToDigit;
 
-// find total cost for redactiong text
+// calculate the cost of text editing
 function calcTextEditCost(text, type = '.docx', lang) {
   let cost_per_symbols,
     minCost,
     totalCost
 
+  // get cost per symbol depending on language
   lang === 'EN' ? minCost = MIN_EN_COST : minCost = MIN_RU_UKR_COST
 
-  if (typeOfFile.some(basicType => basicType === type)) {
+  // calculate & check if we need to add 20% to cost per symbol because of file format
+  if (basicTypesOfFile.some(basicType => basicType === type)) {
     cost_per_symbols = languages[lang]
   } else {
-    cost_per_symbols = Number((languages[lang] + languages[lang] * RISING_PRICE_PERCENT / 100).toFixed(4))
+    cost_per_symbols = Number((languages[lang] + languages[lang] * PERCENT_FOR_EXTRA_RESOURCES / 100).toFixed(4))
   }
 
-  console.log(cost_per_symbols)
-
+  // check if the task can be done on minimal price or calculate total price
   if (text.length * cost_per_symbols < minCost) {
     totalCost = minCost
   } else {
@@ -41,11 +49,115 @@ function calcTextEditCost(text, type = '.docx', lang) {
   return Math.ceil(totalCost) // нужно ли округлять копейки к гривням? я округлила
 }
 
-console.log(calcTextEditCost(text920, '.docx', 'RU'))
+console.log(calcTextEditCost(text1400, '.docx', 'EN'))
 
-const MIN_HOURS_PER_ORDER = 1
-// function calcHours
+// calculate the time of text editing in minutes
+function calcTextEditHours(text, type = ".docx", lang) {
+  let min_per_symbols, 
+      extraTime = 0,
+      totalTimeInMin;
 
-// function calcDate
-// const HOURS_PER_DAY = 8
-// if (!!hours_per_symbols) {
+  // calculete minutes of work depending on language
+  if (lang === 'EN') {
+    min_per_symbols = text.length / SYMBOLS_EN_PER_HOUR * 60
+  } else {
+    min_per_symbols = text.length / SYMBOLS_RU_UKR_PER_HOUR * 60;
+  }
+
+  // check if we need to add 20% to business time because of file format
+  if (!basicTypesOfFile.some((basicType) => basicType === type)) {
+    extraTime = min_per_symbols * PERCENT_FOR_EXTRA_RESOURCES / 100
+  }
+
+  // check if the task can be done on minimal time or calculate total business time
+  if (OPEN_ORDER_PROCESSING_TIME_IN_MIN + extraTime + min_per_symbols < MIN_MINUTES_PER_ORDER) {
+    totalTimeInMin = MIN_MINUTES_PER_ORDER;
+  } else {
+    totalTimeInMin = Math.ceil(OPEN_ORDER_PROCESSING_TIME_IN_MIN + extraTime + min_per_symbols)
+  }
+
+  return totalTimeInMin;
+}
+
+console.log(calcTextEditHours(text1400, ".docx", "EN"));
+
+// calculate the date of deadline for order
+function getDeadlineForOrder(leadTime) {
+  let deadline,
+      deadlineDateInfo,
+      deadlineTimeInfo
+  const startOrderDate = new Date();
+  let orderProcessingDate = new Date(startOrderDate);
+  let COBofStartOrderDay = new Date(new Date(startOrderDate).setHours(19, 0, 0, 0))
+
+  // check if we can deliver the order in the next hour
+  let testDateOrderPerHour = new Date(new Date(orderProcessingDate).getTime() + ONE_HOUR_IN_MIN * 60 * 1000)
+  if (orderProcessingDate.getDay() < 6 && leadTime <= 60 && testDateOrderPerHour < COBofStartOrderDay) {
+    return `Здамо за: одну годину`;
+  }
+
+  // check if we can deliver the order in the next two hours
+  let testDateOrderPerTwoHours = new Date(new Date(orderProcessingDate).getTime() + TWO_HOURS_IN_MIN * 60 * 1000)
+  if (orderProcessingDate.getDay() < 6 && leadTime <= 120 && testDateOrderPerTwoHours < COBofStartOrderDay) {
+    return `Здамо за: дві години`;
+  }
+
+  while (leadTime > 0) {
+    // add 1 hour to orderProcessingDate in business time
+    if (orderProcessingDate.getDay() < 6 && orderProcessingDate.getHours() <= 17) {
+      if (leadTime < 60) {
+        orderProcessingDate = new Date(orderProcessingDate.getTime() + leadTime * 60 * 1000)
+        leadTime -= leadTime;
+        break
+      }
+      orderProcessingDate = new Date(orderProcessingDate.getTime() + 1 * 3600 * 1000);
+    } else if ( // add remaining minutes before COB(close of business) to orderProcessingDate
+        orderProcessingDate.getHours() <= 18 &&
+        orderProcessingDate.getMinutes() < 59
+      ) {
+      leadTime -= 60 - orderProcessingDate.getMinutes()
+      orderProcessingDate = new Date(orderProcessingDate.getTime() + (60 - orderProcessingDate.getMinutes()) * 60 * 1000)
+    } else {
+      // do not count weekends
+      while (orderProcessingDate.getDay() >= 5) {
+        orderProcessingDate = new Date(orderProcessingDate.getTime() + 24 * 3600 * 1000);
+        orderProcessingDate = new Date(orderProcessingDate.setHours(10, 0, 0, 0));
+      }
+      // do not count not business time on weekdays
+      orderProcessingDate = new Date(orderProcessingDate.getTime() + 16 * 3600 * 1000);
+      orderProcessingDate = new Date(orderProcessingDate.setHours(10, 0, 0, 0));
+
+      if (leadTime < 60) {
+        orderProcessingDate = new Date(orderProcessingDate.getTime() + leadTime * 60 * 1000)
+        leadTime -= leadTime;
+        break
+      }
+      orderProcessingDate = new Date(orderProcessingDate.getTime() + 1 * 3600 * 1000);
+    }
+
+    if (leadTime >= 60) {
+      leadTime -= 60;
+    }
+  }
+
+  // Round off the deadline to be a multiple of 30 minutes
+  if (orderProcessingDate.getMinutes() < 30) {
+    deadline = new Date(new Date(orderProcessingDate).setMinutes(30))
+  } else {
+    deadline = new Date(new Date(orderProcessingDate).setHours(orderProcessingDate.getHours() + 1, 0, 0, 0));
+  }
+
+  const deadlineInfo = {
+    day: countDateDigits(deadline.getDate()),
+    month: countDateDigits(deadline.getMonth()),
+    year: deadline.getFullYear().toString().slice(-2),
+    hours: countDateDigits(deadline.getHours()),
+    minutes: countDateDigits(deadline.getMinutes())
+  }
+  deadlineDateInfo = Object.values(deadlineInfo).slice(0, 3).join('.')
+  deadlineTimeInfo = Object.values(deadlineInfo).slice(-2).join(':')
+
+  return `Термін виконання: ${deadlineDateInfo} о ${deadlineTimeInfo}`;
+}
+
+console.log(getDeadlineForOrder(calcTextEditHours(text1400, ".docx", "EN")));
